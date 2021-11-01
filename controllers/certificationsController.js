6f7a63a7d0b2fb6080e938a13ab8ae88eeb92d96
Carlos A <carlosmiranda19122@gmail.com>
2021-11-01 08:33:12 -0300

    testando uploadCertification

--- controllers/certificationsController.js
+++ controllers/certificationsController.js
@@ -27,84 +27,66 @@ exports.uploadCertification = function (req, res) {
   } = req.body;
 
   const {
-    file_name,
-    key_name,
+    originalname: file_name,
+    fieldname: key_name,
     size,
-    url,
+    location: url,
   } = req.file; // SO PRA TENSTAR VOU BOTAR BODY MAS É FILE
 
-  console.log('key-name ->' + req.file.certification_name)
+  // console.log('key-name ->' + )
   const user = new User(req.session.user)
   const mission = new Mission()
   const finishedMission = new FinishedMission()
 
   const uploadedCertification = new UploadedCertification(key_name, size, url, file_name)
 
-  return res.send(req.body)
-  // uploadedCertification.create()
-  //   .then(file_uploaded => {
-  //     console.log(file_uploaded)
+  return res.json({
+    body: {
+      certification_name,
+      description,
+      activity_start,
+      activity_end,
+      amount_hours,
+      id_activity
+    }, file: { file_name, key_name, size, url }
+  })
+  uploadedCertification.create()
+    .then(file_uploaded => {
+      console.log(file_uploaded)
 
-  //     const searched_user = user.readByEmail(req.body.email) // PRA TESTAR VOU TIRAR O 'req.session.user.email'
-  //       .then(result => {
-  //         if (!searched_user) {
-  //           return new Error('User doesnt found')
-  //         }
+      const searched_user = user.readByEmail(req.body.email) // PRA TESTAR VOU TIRAR O 'req.session.user.email'
+        .then(result => {
+          if (!searched_user) {
+            return new Error('User doesnt found')
+          }
 
-  //         console.log(result)
+          console.log(result)
 
-  //         return result
-  //       })
-  //       .catch(err => console.log(err))
+          return result
+        })
+        .catch(err => console.log(err))
 
-  //     const certification = new Certification(certification_name, description, activity_start, activity_end, amount_hours, id_activity, file_uploaded.id,)
-  //     certification.create()
-  //       .then(certification_created => {
+      const certification = new Certification(certification_name, description, activity_start, activity_end, amount_hours, id_activity, file_uploaded.id,)
+      certification.create()
+        .then(certification_created => {
 
-  //         mission.missionValidate(searched_user.id, certification_created.id)
-  //           .then((mission_validated) => {
-  //             console.log(mission_validated)
+          mission.missionValidate(searched_user.id, certification_created.id)
+            .then((mission_validated) => {
+              console.log(mission_validated)
 
-  //             finishedMission.create(mission_validated.id, searched_user.id)
-  //               .then((finished_mission_created) => {
-  //                 user.countAmountPoints(searched_user.points_total_amount, mission_validated.rewards, searched_user.id)
-  //                   .then(
-  //                     res.send("redireciona o usuario para a pagina principal")
-  //                   )
-  //               })
-  //               .catch(err => console.log(err))
-  //           })
-  //           .catch(err => console.log(err))
-  //       }
-  //       )
-  //       .catch(err => console.log(err))
-  //   })
-  //   .catch(err => console.log(err))
-
-  // certification.create()
-  //   .then(certification_created => {
-  //     const searched_user = user.readByEmail(req.session.user.email)
-  //       .then(result => {
-  //         console.log(result)
-
-  //         return result
-  //       })
-  //       .catch(err => console.log("User doesnt found - " + err))
-
-  //     mission.missionValidate(searched_user.id, certification_created.id)
-  //       .then((mission_validated) => {
-  //         console.log(result)
-
-  //         finishedMission.create(mission_validated.id, searched_user.id)
-  //           .then((finished_mission_created) => {
-  //             user.countAmountPoints(searched_user.points_total_amount, mission_validated.rewards, searched_user.id)
-  //               .then(
-  //                 res.send("redireciona o usuario para a pagina principal")
-  //               )
-  //           })
-  //           .catch(err => console.log(err))
-  //       })
-  //       .catch(err => console.log(err))
-  //   }
-  //   )
+              finishedMission.create(mission_validated.id, searched_user.id)
+                .then((finished_mission_created) => {
+                  user.countAmountPoints(searched_user.points_total_amount, mission_validated.rewards, searched_user.id)
+                    .then(
+                      res.send("redireciona o usuario para a pagina principal")
+                    )
+                })
+                .catch(err => console.log(err))
+            })
+            .catch(err => console.log(err))
+        }
+        )
+        .catch(err => console.log(err))
+    })
+    .catch(err => console.log(err))
 }
