aa1ff8999a5aacb9da9f27d1365a820fc5f1edb6
Carlos A <carlosmiranda19122@gmail.com>
2021-11-02 12:00:03 -0300

    activity: adicionado a funçao para buscar uma atividade correspondente ao tipo inserido no certificado

--- models/Activity.js
+++ models/Activity.js
@@ -18,6 +18,25 @@ Activity.prototype.listAllPerSubcategory = () => {
 
 }
 
+Activity.prototype.searchOne = function (id_type) {
+  const select = "SELECT * FROM activities AS act" +
+    " JOIN activity_types AS type ON act.id_type = type.id_activity_types" +
+    " WHERE act.id_type = $1";
+  const values = [id_type];
+
+  return new Promise((resolve, reject) => {
+    pool.query(select, values, (error, results) => {
+      if (error) {
+        reject("E-mail não encontrado");
+      } else {
+        console.log(results.rows[0])
+        resolve(results.rows[0]);
+
+      }
+    });
+  });
+};
+
 Activity.prototype.create = () => {
 
 }
